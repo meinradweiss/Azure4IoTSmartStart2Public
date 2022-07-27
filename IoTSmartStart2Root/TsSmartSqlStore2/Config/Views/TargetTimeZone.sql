61e03fb448cee666f36c1c3fd5061f25fcd714f0
meinradweiss <meinrad.weiss@hotmail.com>
2022-07-27 10:43:15 +0200

    [Config].[TargetTimeZone] adjusted to allow an empty default configuration

--- IoTSmartStart2Root/TsSmartSqlStore2/Config/Views/TargetTimeZone.sql
+++ IoTSmartStart2Root/TsSmartSqlStore2/Config/Views/TargetTimeZone.sql
@@ -1,7 +1,6 @@
 ﻿CREATE VIEW [Config].[TargetTimeZone]
 AS
 
-  SELECT COALESCE(CONVERT(NVARCHAR, [SystemConfig].[SystemConfigValue])
+
+  SELECT COALESCE((SELECT CONVERT(NVARCHAR, [SystemConfigValue]) FROM  [Config].[SystemConfig] WHERE [SystemConfig].[SystemConfigName] = 'LocalTimezone')
                         ,'Central European Standard Time')                AS TargetTimeZone
-  FROM  [Config].[SystemConfig] 
-  WHERE [SystemConfig].[SystemConfigName] = 'LocalTimezone'
